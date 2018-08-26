d802aaad438ce25fc6350f2aff480f1df4dc3a5a
Alpha Tan <534332648@qq.com>
2018-08-26 22:22:07 +0800

    fix blog load bug

--- js/blogList.js
+++ js/blogList.js
@@ -1,22 +1,26 @@
 // JavaScript Document
-blogMap = new Map()
 
 function loadBlogs() {
-	var blogPrefix = "../blog/"
+	var blogPrefix = "https://alphablacktan.github.io/blog/"
 	var blogPaths = new Array(
 		"BulkLoad Can not create a path from a null string.md",
 		"一小时入手ANT脚本.md"
 	)
 	
 	for(var i = 0; i < blogPaths.length; i ++) {
-		var blogPath = blogPrefix + blogPaths[i]
+		var blogPath = encodeURI(blogPrefix + blogPaths[i])
 		$.get(blogPath, function(result){
-			blogMap.set(blogPath, result)
+			blogMap.set(blogPaths[i], result)
 		});
 	}
 }
 
+
+var blogMap = new Map()
+
 window.onload=function(){
+	$.ajaxSetup({
+		async:false
+	});
 	loadBlogs()
-	alert(blogMap.get("BulkLoad Can not create a path from a null string.md"))
 }
