9d6311b9a88e7cf4ddd9bace0164b32428df16d5
Alpha Tan <534332648@qq.com>
2018-08-26 21:23:17 +0800

    fix getBlog bug

--- js/blogList.js
+++ js/blogList.js
@@ -1,38 +1,22 @@
 // JavaScript Document
+blogMap = new Map()
 window.onload=function(){
-	var blogPaths = loadBlogs()
-	var blogs = getBlogs(blogPaths)
-	alert(blogs)
+	loadBlogs()
+	alert(blogMap.get("BulkLoad Can not create a path from a null string.md"))
+	alert(blogMap.get("一小时入手ANT脚本.md"))
 }
 
 function loadBlogs() {
 	var blogPrefix = "../blog/"
-	var blogs = new Array(
+	var blogPaths = new Array(
 		"BulkLoad Can not create a path from a null string.md",
 		"一小时入手ANT脚本.md"
 	)
 	
 	for(var i = 0; i < blogs.length; i ++) {
-		blogs[i] = blogPrefix + blogs[i]
+		var blogPath = blogPrefix + blogs[i]
+		$.get(blogPath, function(result){
+			blogMap.set(blogPath, result)
+		});
 	}
-	
-	return blogs
-}
-
-function getBlogs(blogPaths) {
-	var blogs = new Array(blogPaths.length)
-	
-	for(var i = 0; i < blogs.length; i ++) {
-		blogs[i] = openBlog(blogPaths[i])
-	}
-	return blogs
-}
-	
-function openBlog(blogPath) {
-	var data;
-	$.get(blogPath, function(result){
-		data = result;
-	});
-	
-	return data;
 }
